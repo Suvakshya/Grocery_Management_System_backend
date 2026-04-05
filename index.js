0ef43a10a4c4313c36699f2c4df0f13d02c24d2b
Suvakshya <asuvaksha@gmail.com>
2026-04-05 14:35:17 +0545

    Changes

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const app = express();
 
 await connectCloudinary();
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://grocery-management-system-frontend.vercel.app/"
+];
 //middlewares
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
